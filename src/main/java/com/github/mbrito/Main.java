652524c47cb74533c40b3e305ded9da85d62deca
mbrito <britomauricio2003@gmail.com>
2024-09-24 04:15:51 -0300

    feat: implement main logic

--- src/main/java/com/github/mbrito/Main.java
+++ src/main/java/com/github/mbrito/Main.java
@@ -1,7 +1,16 @@
 package com.github.mbrito;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int[] weights = new int[x];
+
+        for (int i = 0; i < x; i++)
+            weights[i] = scanner.nextInt();
+
+        System.out.println(Balancer.canBalance(weights));
     }
 }
