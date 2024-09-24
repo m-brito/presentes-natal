9b79e3c69c3b2a161621c6b04883e3f19438b4b9
mbrito <britomauricio2003@gmail.com>
2024-09-24 04:15:18 -0300

    feat: implement balancing logic

--- src/main/java/com/github/mbrito/Balancer.java
+++ src/main/java/com/github/mbrito/Balancer.java
@@ -2,6 +2,18 @@ package com.github.mbrito;
 
 public class Balancer {
     public static String canBalance(int[] weights) {
-        return "";
+        if(weights == null) throw new NullPointerException();
+
+        int sideA = 0;
+        int sideB = 0;
+
+        for (int weight: weights){
+            if (weight < 0) throw new IllegalArgumentException();
+
+            if((sideA + weight) - sideB <= 5) sideA += weight;
+            else if ((sideB + weight) - sideA <= 5) sideB += weight;
+            else return("N");
+        }
+        return "S";
     }
 }
