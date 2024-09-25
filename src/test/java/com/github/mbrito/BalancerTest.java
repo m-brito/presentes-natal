078fbf3fbab8580784a92fada1d44fd1983fbafd
mbrito <britomauricio2003@gmail.com>
2024-09-24 21:22:10 -0300

    build: update dependencies `JUnit 4` to `JUnit 5`

--- src/test/java/com/github/mbrito/BalancerTest.java
+++ src/test/java/com/github/mbrito/BalancerTest.java
@@ -1,10 +1,10 @@
 package com.github.mbrito;
 
-import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class BalancerTest {
 
